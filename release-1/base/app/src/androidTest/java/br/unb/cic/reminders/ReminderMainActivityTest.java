f2bd576aa2186905facc22f459edb538c9b2f84d
LPignata <luanpignat@gmail.com>
2018-07-04 16:53:41 -0300

    Adicionado alguns testes

---
+++ release-1/base/app/src/androidTest/java/br/unb/cic/reminders/ReminderMainActivityTest.java
@@ -0,0 +1,313 @@
+package br.unb.cic.reminders;
+
+
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.unb.cic.reminders2.R;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ReminderMainActivityTest {
+
+    @Rule
+    public ActivityTestRule<ReminderMainActivity> mActivityTestRule = new ActivityTestRule(ReminderMainActivity.class);
+
+    @Test
+    public void reminderMainActivityTest() {
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.menu_addReminder), withContentDescription("Add"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.android.internal.widget.ActionBarView")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.edtReminder),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                1)));
+        editText.perform(scrollTo(), replaceText("Sem data"), closeSoftKeyboard());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.btnSave), withText("Save"),
+                        childAtPosition(
+                                allOf(withId(R.id.linearLayout1),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                8)),
+                                0)));
+        button.perform(scrollTo(), click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.txtReminder), withText("Sem data"),
+                        childAtPosition(
+                                allOf(withId(R.id.LinearLayout01),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("Sem data")));
+
+        ViewInteraction actionMenuItemView2 = onView(
+                allOf(withId(R.id.menu_addReminder), withContentDescription("Add"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.android.internal.widget.ActionBarView")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView2.perform(click());
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.edtReminder),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                1)));
+        editText2.perform(scrollTo(), click());
+
+        ViewInteraction editText3 = onView(
+                allOf(withId(R.id.edtReminder),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                1)));
+        editText3.perform(scrollTo(), replaceText("Antigo sem hora"), closeSoftKeyboard());
+
+        ViewInteraction editText4 = onView(
+                allOf(withId(R.id.edtDetails),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        editText4.perform(scrollTo(), replaceText("Description"), closeSoftKeyboard());
+
+        ViewInteraction spinner = onView(
+                allOf(withId(R.id.spinnerDate),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                5)));
+        spinner.perform(scrollTo(), click());
+
+        DataInteraction textView2 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(1);
+        textView2.perform(click());
+
+        ViewInteraction button2 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        button2.perform(click());
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.btnSave), withText("Save"),
+                        childAtPosition(
+                                allOf(withId(R.id.linearLayout1),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                8)),
+                                0)));
+        button3.perform(scrollTo(), click());
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.txtReminder), withText("Antigo sem hora"),
+                        childAtPosition(
+                                allOf(withId(R.id.LinearLayout01),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        textView3.check(matches(withText("Antigo sem hora")));
+
+        ViewInteraction actionMenuItemView3 = onView(
+                allOf(withId(R.id.menu_addReminder), withContentDescription("Add"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.android.internal.widget.ActionBarView")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView3.perform(click());
+
+        ViewInteraction editText5 = onView(
+                allOf(withId(R.id.edtReminder),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                1)));
+        editText5.perform(scrollTo(), replaceText("Antigo com hora"), closeSoftKeyboard());
+
+        ViewInteraction editText6 = onView(
+                allOf(withId(R.id.edtReminder), withText("Antigo com hora"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                1)));
+        editText6.perform(pressImeActionButton());
+
+        ViewInteraction editText7 = onView(
+                allOf(withId(R.id.edtDetails),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        editText7.perform(scrollTo(), replaceText("Uma descricao qualquer"), closeSoftKeyboard());
+
+        ViewInteraction editText8 = onView(
+                allOf(withId(R.id.edtDetails), withText("Uma descricao qualquer"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        editText8.perform(pressImeActionButton());
+
+        ViewInteraction spinner2 = onView(
+                allOf(withId(R.id.spinnerDate),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                5)));
+        spinner2.perform(scrollTo(), click());
+
+        DataInteraction textView4 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(1);
+        textView4.perform(click());
+
+        ViewInteraction button4 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        button4.perform(click());
+
+        ViewInteraction spinner3 = onView(
+                allOf(withId(R.id.spinnerTime),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                7)));
+        spinner3.perform(scrollTo(), click());
+
+        DataInteraction textView5 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(1);
+        textView5.perform(click());
+
+        ViewInteraction button5 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        button5.perform(click());
+
+        ViewInteraction button6 = onView(
+                allOf(withId(R.id.btnSave), withText("Save"),
+                        childAtPosition(
+                                allOf(withId(R.id.linearLayout1),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                8)),
+                                0)));
+        button6.perform(scrollTo(), click());
+
+        ViewInteraction textView6 = onView(
+                allOf(withId(R.id.txtReminder), withText("Antigo com hora"),
+                        childAtPosition(
+                                allOf(withId(R.id.LinearLayout01),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        textView6.check(matches(withText("Antigo com hora")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
