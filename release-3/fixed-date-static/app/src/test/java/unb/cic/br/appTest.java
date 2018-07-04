cf97cf9e1dd7699e372e822b4547970bd1ce1290
LPignata <luanpignata@aluno.unb.br>
2018-07-04 10:11:15 -0300

    Agora foi

---
+++ release-3/fixed-date-static/app/src/test/java/unb/cic/br/appTest.java
@@ -0,0 +1,13 @@
+package unb.cic.br;
+
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class appTest {
+    @Test
+    public void firstTest() {
+        assertFalse(1 < 0);
+    }
+}
